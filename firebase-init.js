e47c3b19c5a149258de30b6c433080e1abcc5304
Zuhdi Zahdeh <zuhdi.zahdeh@gmail.com>
2025-05-23 19:09:25 +0300

    Update firebase-init.js
    change the firebaseConfig

--- firebase-init.js
+++ firebase-init.js
@@ -2,13 +2,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDPl_dVt3W6sJz_hLy9Z2Izb8ODKKnPaB4",
-    authDomain: "students-platform-1d465.firebaseapp.com",
-    projectId: "students-platform-1d465",
-    storageBucket: "students-platform-1d465.firebasestorage.app",
-    messagingSenderId: "652495188649",
-    appId: "1:652495188649:web:2a3f0bfe40234b3ec1afb8"
+  apiKey: "AIzaSyDPl_dVt3W6sJz_hLy9Z2Izb8ODKKnPaB4",
+  authDomain: "students-platform-1d465.firebaseapp.com",
+  projectId: "students-platform-1d465",
+  storageBucket: "students-platform-1d465.firebasestorage.app",
+  messagingSenderId: "652495188649",
+  appId: "1:652495188649:web:2a3f0bfe40234b3ec1afb8",
+  measurementId: "G-V4BN36PEZT"
 };
 
 const app = initializeApp(firebaseConfig);
